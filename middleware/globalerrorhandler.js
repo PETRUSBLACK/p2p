d3068cc4412d8057177b558c8f80e9a7cf263e49
Osigelialex <osigelialex@gmail.com>
2024-04-09 13:12:58 +0100

    remove error stack from error handler in production

--- middleware/globalerrorhandler.js
+++ middleware/globalerrorhandler.js
@@ -1,10 +1,8 @@
 export const globalError = (err, req, res, next) => {
-    const stack = err?.stack;
     const message = err?.message;
     const statusCode = err?.statusCode ? err?.statusCode : 500;
   
     res.status(statusCode).json({
-      stack,
       message,
     });
   };
