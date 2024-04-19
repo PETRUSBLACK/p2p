73445326c0c55afa763803c3b1bb09f9f71b2a10
Osigelialex <osigelialex@gmail.com>
2024-04-19 11:06:56 +0100

    changed redirect url to p2p domain

--- config/oauth.js
+++ config/oauth.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/v1/auth/google/callback",
+      callbackURL: "https://p2p-qrjp.onrender.com/api/v1/auth/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
