2250afad67b0e9a45f4cc4217d24ec7bcd2ca621
Irechukwu Confidence <irechukwuconfidence@mail.com>
2024-05-25 20:54:24 +0100

    done with image and user profile update

--- models/User.js
+++ models/User.js
@@ -28,12 +28,6 @@ const UserSchema = new schema({
         minlength: [3, 'Username must be 3 letters long'],
         unique: true
     },
-    // profile_img: {
-    //     type: String,
-    //     default: () => {
-    //         return `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`
-    //     }
-    // },
     profile_img: {
         type: String,
         default: () => {
