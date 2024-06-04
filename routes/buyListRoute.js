11ebfcd7f1ddd20e867c921ff2d5f5bd21cdffc9
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-06-04 09:50:17 +0100

    documentation

--- routes/buyListRoute.js
+++ routes/buyListRoute.js
@@ -6,58 +6,41 @@ const buyListRoutes = express.Router();
 
 /**
  * @swagger
- * components:      
- *    securitySchemes:
- *      bearerAuth:
- *          type: http
- *          scheme: bearer
- *          bearerFormat: JWT
- *    schemas:
- *      BuyList:
- *          type: object
- *          properties:
- *              cryptoCurrencyName:
- *                  type: string
- *              fiatCurrency:
- *                  type: string
- *              pricePerCoin:
- *                  type: string
- *              totalAmountOfCrypto:
- *                  type: string
- *              rangeMin:
- *                  type: string
- *              rangeMax:
- *                  type: string
- *              paymentTimeLimit:
- *                  type: string
- *              fee:
- *                  type: string
- *              details:
- *                  type: string
- * 
- * /api/v1/buyList/:
+ * /api/v1/users/register:
  *   post:
- *     summary: Permits users to create a buy list
- *     description: This is the endpoint to create a buy list
+ *     summary: Create otp for user registration
+ *     description: This is the endpoint to create an otp for a user which the user will verify and be registered succesfully
  *     tags:
- *       - buyList
- *     security:
- *       - bearerAuth: []
+ *       - users
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/BuyList'
+ *             $ref: '#/components/schemas/User'
  *     responses:
  *       '201':
- *         description: Successful
+ *         description: Please check your email and sms for your otp's
  *       '400':
- *         description: Bad request
+ *         description: Bad request. Invalid email format.
  *       '409':
- *         description: Conflict
+ *         description: Conflict. User with provided email already exists.
  *       '500':
  *         description: Internal server error
+ *
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         fullname:
+ *           type: string
+ *         password:
+ *           type: string
+ *         email:
+ *           type: string
+ *         phone:
+ *           type: string
  */
 
 buyListRoutes.post("/", isLoggedIn, createBuyListValidationRules(), createBuyList);
